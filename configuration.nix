a3dbb9d9562de54a3025e41ff765cc3e2aea43eb
Lucien <lucien.bastin@protonmail.com>
2025-10-20 23:17:40 +0200

    First commit with hyprland and kitty setup

---
+++ configuration.nix
@@ -0,0 +1,66 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Paris";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "fr";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  services.xserver = {
+    xkb.layout = "fr";
+  }; 
+
+  # Enable sound.
+  # services.pulseaudio.enable = true;
+  # OR
+  # services.pipewire = {
+  #   enable = true;
+  #   pulse.enable = true;
+  # };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  users.users.lucien = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  programs.firefox.enable = true;
+
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    xwayland.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    kitty
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  system.stateVersion = "25.05";
+
+}
+
---
+++ home.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ...}:
+
+{
+  home.username = "lucien";
+  home.homeDirectory = "/home/lucien";
+  home.stateVersion = "25.05";
+  
+  programs.git = {
+    enable = true;
+    userName = "Lucien";
+    userEmail = "lucien.bastin@protonmail.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
+
+  programs.bash = {
+    enable = true;
+    profileExtra = ''
+      if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
+        exec uwsm start -S hyprland-uwsm.desktop
+      fi
+    '';
+  };
+
+  home.file.".config/kitty".source = ./config/kitty;
+  home.file.".config/hypr".source = ./config/hypr;
+}
