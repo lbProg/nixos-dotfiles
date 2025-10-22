b3a7cfa5c8a0694ce869111c9c2e78cfb7f3830b
Lucien <lucien.bastin@protonmail.com>
2025-10-23 01:11:33 +0200

    Autostart waybar and Bibata cursor theme

--- nix/home.nix
+++ nix/home.nix
@@ -12,6 +12,7 @@ in
   home.packages = with pkgs; [
     btop
     nixpkgs-fmt
+    waybar
   ];
 
   programs.git = {
@@ -34,6 +35,17 @@ in
         exec uwsm start -S hyprland-uwsm.desktop
       fi
     '';
+  };
+
+  # gtk.cursorTheme.Package = pkgs.bibata-cursors;
+  # gtk.cursorTheme.Name = "Bibata-Modern-Ice";
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    name = "Bibata-Modern-ice";
+    package = pkgs.bibata-cursors;
+    size = 24;
   };
 
   home.sessionPath = [ "/home/lucien/nixos-dotfiles/bin" ];
