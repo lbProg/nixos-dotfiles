06d15f8681fc1f6ea02516d74ec10e05bea770fe
Lucien <lucien.bastin@protonmail.com>
2025-10-24 01:56:32 +0200

    Trying to autoreload neovim theme after change

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,2 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+dofile("/home/lucien/.config/theming/current/theme/neovim.lua")
---
+++ config/nvim/lua/plugins/load-theme.lua
@@ -0,0 +1,5 @@
+local theme_path = "/home/lucien/.config/theming/current/theme/neovim.lua"
+
+local theme = dofile(theme_path)
+
+return theme
